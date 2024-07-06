1df759a3855a3e872ebffded059249e62cc16383
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2024-07-06 16:31:15 +0200

    Wraped previosuly created components - MainMenu, Logo, CurrencySelector, IconMenu

--- front-end/src/components/Layout/Layout.jsx
+++ front-end/src/components/Layout/Layout.jsx
@@ -3,14 +3,17 @@ import { Footer } from '../Footer/Footer'
 import { IconMenu } from '../IconMenu/IconMenu'
 import { Logo } from '../Logo/Logo'
 import { MainMenu } from '../MainMenu/MainMenu'
+import { TopBar } from '../TopBar/TopBar'
 
 export function Layout() {
   return (
     <>
-      <MainMenu />
-      <Logo />
-      <CurrencySelector />
-      <IconMenu />
+      <TopBar>
+        <MainMenu />
+        <Logo />
+        <CurrencySelector />
+        <IconMenu />
+      </TopBar>
       <Footer />
     </>
   )
