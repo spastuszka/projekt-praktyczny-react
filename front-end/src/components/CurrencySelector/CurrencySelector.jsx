1a8638478ba3b300d7f60382481a6e48b4aec0d8
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2024-07-06 15:56:34 +0200

    Added currency selector style and improted

--- front-end/src/components/CurrencySelector/CurrencySelector.jsx
+++ front-end/src/components/CurrencySelector/CurrencySelector.jsx
@@ -1,8 +1,9 @@
+import styles from './CurrencySelector.module.css'
 import { CURRENCIES } from '../../constants/currencies'
 
 export function CurrencySelector() {
   return (
-    <select>
+    <select className={styles.currencySelector}>
       <option value={CURRENCIES.PLN}>{CURRENCIES.PLN}</option>
       <option value={CURRENCIES.USD}>{CURRENCIES.USD}</option>
     </select>
