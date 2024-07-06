ea811cf5abd4d90823a12a87a85234c8e051bc1d
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2024-07-06 15:06:33 +0200

    imported GENDERS and use it instead of normal gender array

--- front-end/src/components/MainMenu/MainMenu.jsx
+++ front-end/src/components/MainMenu/MainMenu.jsx
@@ -1,24 +1,11 @@
 import styles from './MainMenu.module.css'
+import { GENDERS } from '../../constants/categories'
 import { NavLink } from 'react-router-dom'
 
 export function MainMenu() {
-  const genders = [
-    {
-      categoryName: 'Kobieta',
-      path: 'kobieta',
-    },
-    {
-      categoryName: 'Mężczyzna',
-      path: 'mezczyzna',
-    },
-    {
-      categoryName: 'Dziecko',
-      path: 'dziecko',
-    },
-  ]
   return (
     <ul className={styles.mainMenu}>
-      {genders.map((category) => {
+      {GENDERS.map((category) => {
         return (
           <li key={category.path}>
             <NavLink to={category.path}>{category.categoryName}</NavLink>
