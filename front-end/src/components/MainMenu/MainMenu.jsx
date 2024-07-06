6fc15f915650f9aeae45b6b44a87c46cb23898c8
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2024-07-06 14:12:34 +0200

    Added declaration Main Menu components strucuter and empty module file

---
+++ front-end/src/components/MainMenu/MainMenu.jsx
@@ -0,0 +1,29 @@
+import { NavLink } from 'react-router-dom'
+
+export function MainMenu() {
+  const genders = [
+    {
+      categoryName: 'Kobieta',
+      path: 'kobieta',
+    },
+    {
+      categoryName: 'Mężczyzna',
+      path: 'mezczyzna',
+    },
+    {
+      categoryName: 'Dziecko',
+      path: 'dziecko',
+    },
+  ]
+  return (
+    <ul>
+      {genders.map((category) => {
+        return (
+          <li key={category.path}>
+            <NavLink to={category.path}>{category.categoryName}</NavLink>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
