89f4675f1434ce813f43867dc7f5cdf4cfca2e55
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2024-07-06 15:11:53 +0200

    Added import Logo component into Layout

--- front-end/src/components/Layout/Layout.jsx
+++ front-end/src/components/Layout/Layout.jsx
@@ -1,10 +1,12 @@
 import { Footer } from '../Footer/Footer'
+import { Logo } from '../Logo/Logo'
 import { MainMenu } from '../MainMenu/MainMenu'
 
 export function Layout() {
   return (
     <>
       <MainMenu />
+      <Logo />
       <Footer />
     </>
   )
