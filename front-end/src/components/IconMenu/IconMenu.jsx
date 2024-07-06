59fa5a4afc7620512fddc6d3b15390d2a3b02f2c
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2024-07-06 16:23:12 +0200

    Implemented and style of numberOfProdcut next to bag icon

--- front-end/src/components/IconMenu/IconMenu.jsx
+++ front-end/src/components/IconMenu/IconMenu.jsx
@@ -1,11 +1,13 @@
+import styles from './IconMenu.module.css'
 import BAG_ICON from '../../assets/bag.svg'
-import HEART from '../../assets/bag.svg'
+import HEART from '../../assets/heart.svg'
 
 import { Link } from 'react-router-dom'
 
 export function IconMenu() {
+  const cartItems = 2
   return (
-    <ul>
+    <ul className={styles.iconMenu}>
       <li>
         <Link to="/ulubione">
           <img src={HEART} />
@@ -14,6 +16,7 @@ export function IconMenu() {
       <li>
         <Link to="/koszyk">
           <img src={BAG_ICON} />
+          <div className={styles.numberOfProducts}>{cartItems}</div>
         </Link>
       </li>
     </ul>
