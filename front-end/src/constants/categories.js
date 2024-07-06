e56ceb5ccc4d14789880c021e249888d9477192c
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2024-07-06 15:05:43 +0200

    Added GENDERS constant array

---
+++ front-end/src/constants/categories.js
@@ -0,0 +1,14 @@
+export const GENDERS = [
+  {
+    categoryName: 'Kobieta',
+    path: 'kobieta',
+  },
+  {
+    categoryName: 'Mężczyzna',
+    path: 'mezczyzna',
+  },
+  {
+    categoryName: 'Dziecko',
+    path: 'dziecko',
+  },
+]
