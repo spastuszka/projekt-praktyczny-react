b526c99e5153f5ae31b83615c640f8d2edf734cc
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2024-07-07 13:57:18 +0200

    Added favourites style

--- front-end/src/components/FavouritesList/FavouritesList.jsx
+++ front-end/src/components/FavouritesList/FavouritesList.jsx
@@ -1,14 +1,17 @@
+import styles from './FavouritesList.module.css'
 import { CenteredContent } from '../CenteredContent/CenteredContent'
 import { FavouritesProduct } from '../FavouritesProduct/FavouritesProduct'
 
 export function FavouritesList({ products }) {
   return (
     <CenteredContent>
-      <h2>Ulubione</h2>
-      <div>
-        {products.map((product) => {
-          return <FavouritesProduct key={product.id} product={product} />
-        })}
+      <div className={styles.favouritesList}>
+        <h2>Ulubione</h2>
+        <div>
+          {products.map((product) => {
+            return <FavouritesProduct key={product.id} product={product} />
+          })}
+        </div>
       </div>
     </CenteredContent>
   )
