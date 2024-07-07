b4b6ce34aabc3dac4298bbb7733723b20b00c04b
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2024-07-07 13:14:07 +0200

    Added routing to react routing for favourites page

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -5,11 +5,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { MainPage } from './views/MainPage/MainPage.jsx'
+import { Favourites } from './views/Favourites/Favourites.jsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <MainPage />,
+  },
+  {
+    path: 'ulubione',
+    element: <Favourites />,
   },
 ])
 
