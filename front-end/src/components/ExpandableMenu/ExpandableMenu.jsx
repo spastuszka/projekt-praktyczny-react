9fa3fd7df35b078753e23147d97ff814d05f1ec5
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2024-07-18 20:22:30 +0200

    Added ExpandableMenu component

---
+++ front-end/src/components/ExpandableMenu/ExpandableMenu.jsx
@@ -0,0 +1,33 @@
+import { CATEGORIES } from '../../constants/categories'
+import { NavLink } from 'react-router-dom'
+import ARROW_ICON from '../../assets/arrow.svg'
+
+export function ExpandableMenu() {
+  return (
+    <div>
+      <p>Kobieta</p>
+      <ul>
+        {CATEGORIES.map((category) => {
+          return (
+            <li key={category.path}>
+              <NavLink to={category.path}>
+                {category.categoryName} <img src={ARROW_ICON} />
+              </NavLink>
+              <ul>
+                {category.subcategories.map((subcategory) => {
+                  return (
+                    <li key={subcategory.path}>
+                      <NavLink to={subcategory.path}>
+                        {subcategory.categoryName}
+                      </NavLink>
+                    </li>
+                  )
+                })}
+              </ul>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
