b37b18d1d0789f0e7884070933c2d71c87539baa
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2024-08-09 08:42:54 +0200

    Added brand in product details

--- front-end/src/components/Details/Details.jsx
+++ front-end/src/components/Details/Details.jsx
@@ -2,4 +2,10 @@ import styles from './Details.module.css'
 import CART_ICON from '../../assets/car.svg'
 import RETURN_ICON from '../../assets/return.svg'
 
-export function Details({ product }) {}
+export function Details({ product }) {
+  return (
+    <div>
+      <h2>{product.brand}</h2>
+    </div>
+  )
+}
