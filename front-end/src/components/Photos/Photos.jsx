ff40a1959a02cba89a1188d43c9cf916a853c1a7
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2024-08-09 08:15:00 +0200

    Added state to change main photo

--- front-end/src/components/Photos/Photos.jsx
+++ front-end/src/components/Photos/Photos.jsx
@@ -1,15 +1,23 @@
 import styles from './Photos.module.css'
 import { FlexContainer } from '../FlexContainer/FlexContainer'
+import { useState } from 'react'
 
 export function Photos({ product }) {
+  const [currentPhoto, setCurrentPhoto] = useState(product.photos[0])
   return (
     <FlexContainer>
       <div className={styles.thumbnails}>
         {product.photos.map((photo) => {
-          return <img key={photo} src={photo} />
+          return (
+            <img
+              onClick={() => setCurrentPhoto(photo)}
+              key={photo}
+              src={photo}
+            />
+          )
         })}
       </div>
-      <img className={styles.mainPhoto} src={product.photos[0]} />
+      <img className={styles.mainPhoto} src={currentPhoto} />
     </FlexContainer>
   )
 }
