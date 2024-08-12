bc49c6e73f9d7c73b8e40f42345e6d812e2fec36
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2024-08-12 15:17:18 +0200

    Added product name, price and button in component details

--- front-end/src/components/Details/Details.jsx
+++ front-end/src/components/Details/Details.jsx
@@ -1,11 +1,15 @@
 import styles from './Details.module.css'
 import CART_ICON from '../../assets/car.svg'
 import RETURN_ICON from '../../assets/return.svg'
+import { FullWidthButton } from '../FullWidthButton/FullWidthButton'
 
 export function Details({ product }) {
   return (
     <div>
       <h2>{product.brand}</h2>
+      <p>{product.productName}</p>
+      <p>{product.pricePLN}</p>
+      <FullWidthButton>Dodaj do koszyka</FullWidthButton>
     </div>
   )
 }
