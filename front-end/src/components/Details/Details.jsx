0a71512494b7497dbeefbed8017eda8691e6c74f
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2024-08-12 15:19:53 +0200

    Added delivery and return info in details

--- front-end/src/components/Details/Details.jsx
+++ front-end/src/components/Details/Details.jsx
@@ -1,5 +1,5 @@
 import styles from './Details.module.css'
-import CART_ICON from '../../assets/car.svg'
+import CAR_ICON from '../../assets/car.svg'
 import RETURN_ICON from '../../assets/return.svg'
 import { FullWidthButton } from '../FullWidthButton/FullWidthButton'
 
@@ -9,7 +9,17 @@ export function Details({ product }) {
       <h2>{product.brand}</h2>
       <p>{product.productName}</p>
       <p>{product.pricePLN}</p>
-      <FullWidthButton>Dodaj do koszyka</FullWidthButton>
+      <FullWidthButton isBlack={true}>Dodaj do koszyka</FullWidthButton>
+      <ul>
+        <li>
+          <img src={CAR_ICON} />
+          Dostawa do 24h
+        </li>
+        <li>
+          <img src={RETURN_ICON} />
+          Zwrot do 100 dni!
+        </li>
+      </ul>
     </div>
   )
 }
