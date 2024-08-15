8af12f1c8e0959152a348b43e2dec8602c40d697
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2024-08-15 14:09:47 +0200

    Api constans variable evaluated to separated file

--- front-end/src/api/mainPageLoader.js
+++ front-end/src/api/mainPageLoader.js
@@ -1,12 +1,7 @@
 import { redirect } from 'react-router-dom'
+import { PATH_TO_ENDPOINT_MAPPING } from '../constants/api'
 
 export function mainPageLoader({ params }) {
-  const PATH_TO_ENDPOINT_MAPPING = {
-    kobieta: 'women',
-    mezczyzna: 'men',
-    dziecko: 'children',
-  }
-
   const backEndPath = PATH_TO_ENDPOINT_MAPPING[params.gender]
   if (backEndPath) {
     return fetch(`http://localhost:3000/${backEndPath}`)
---
+++ front-end/src/constants/api.js
@@ -0,0 +1,5 @@
+export const PATH_TO_ENDPOINT_MAPPING = {
+  kobieta: 'women',
+  mezczyzna: 'men',
+  dziecko: 'children',
+}
