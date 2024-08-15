5dc06526071873938baee23cba61cdec02a0c60f
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2024-08-15 09:54:55 +0200

    Added accordion tiem element structure

--- front-end/src/components/Accordion/Accordion.jsx
+++ front-end/src/components/Accordion/Accordion.jsx
@@ -1,3 +1,18 @@
-export function Accordion() {
-  return <></>
+import ARROW_ICON from '../../assets/arrow.svg'
+export function Accordion({ items }) {
+  return (
+    <ul>
+      {items.map((item) => {
+        return (
+          <li key={item.title}>
+            <div>
+              <p>{item.title}</p>
+              <img src={ARROW_ICON} />
+            </div>
+            <p>{item.content}</p>
+          </li>
+        )
+      })}
+    </ul>
+  )
 }
