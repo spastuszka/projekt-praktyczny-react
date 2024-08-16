cf8a2971244c9832a1d236ca34f0980821127454
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2024-08-16 15:06:45 +0200

    Modified foundCategory

--- front-end/src/api/productListLoader.js
+++ front-end/src/api/productListLoader.js
@@ -4,10 +4,9 @@ import { redirect } from 'react-router-dom'
 
 export function productListLoader({ params: { gender, category } }) {
   //Sprawdzenie, czy ścieżka zdefiniowanej kategorii jest równa wartości parametru category przekazanej przez loader przez router
-  const foundCategory = CATEGORIES.find((c) => {
-    c.path === category
-  })
+  const foundCategory = CATEGORIES.find((c) => c.path === category)
   const foundGender = PATH_TO_ENDPOINT_MAPPING[gender]
+
   if (foundGender && foundCategory) {
     return fetch(
       `${BACK_END_URL}/products/?gender=${foundGender}&category=${category}`
