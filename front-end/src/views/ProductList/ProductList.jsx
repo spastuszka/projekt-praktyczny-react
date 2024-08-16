ed956e561168c9bc8f3a129799fd826ab267b1f5
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2024-08-16 15:10:49 +0200

    Fix category header title

--- front-end/src/views/ProductList/ProductList.jsx
+++ front-end/src/views/ProductList/ProductList.jsx
@@ -3,16 +3,21 @@ import { ExpandableMenu } from '../../components/ExpandableMenu/ExpandableMenu'
 import { Breadcrumbs } from '../../components/Breadcrumbs/Breadcrumbs'
 import { Products } from '../../components/Products/Products'
 import { Pagination } from '../../components/Pagination/Pagination'
-import { useLoaderData } from 'react-router-dom'
+import { useLoaderData, useParams } from 'react-router-dom'
+import { CATEGORIES } from '../../constants/categories'
 
 export function ProductList() {
   const products = useLoaderData()
+  const params = useParams()
+
+  const foundCategory = CATEGORIES.find((c) => c.path === params.category)
+
   return (
     <FlexContainer>
       <ExpandableMenu />
       <div>
         <Breadcrumbs />
-        <Products headerText="Swetry" products={products} />
+        <Products headerText={foundCategory.categoryName} products={products} />
         <Pagination numberOfPages={5} />
       </div>
     </FlexContainer>
