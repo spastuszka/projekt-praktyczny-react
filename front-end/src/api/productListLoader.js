8be162f3af02cc1b887dc1477e0431ab869ecf67
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2024-08-16 16:00:12 +0200

    Added logic about laoder subcategory find

--- front-end/src/api/productListLoader.js
+++ front-end/src/api/productListLoader.js
@@ -2,15 +2,26 @@ import { PATH_TO_ENDPOINT_MAPPING, BACK_END_URL } from '../constants/api'
 import { CATEGORIES } from '../constants/categories'
 import { redirect } from 'react-router-dom'
 
-export function productListLoader({ params: { gender, category } }) {
+export function productListLoader({
+  params: { gender, category, subcategory },
+}) {
   //Sprawdzenie, czy ścieżka zdefiniowanej kategorii jest równa wartości parametru category przekazanej przez loader przez router
   const foundCategory = CATEGORIES.find((c) => c.path === category)
   const foundGender = PATH_TO_ENDPOINT_MAPPING[gender]
 
   if (foundGender && foundCategory) {
-    return fetch(
-      `${BACK_END_URL}/products/?gender=${foundGender}&category=${category}`
-    )
+    let url = `${BACK_END_URL}/products/?gender=${foundGender}&category=${category}`
+    if (subcategory) {
+      const foundSubcategory = foundCategory.subcategories.find(
+        (sc) => sc.path === subcategory
+      )
+      if (foundSubcategory) {
+        url = `${url}&subcategory=${subcategory}`
+      } else {
+        return redirect('/kobieta')
+      }
+    }
+    return fetch(url)
   } else {
     return redirect('/kobieta')
   }
