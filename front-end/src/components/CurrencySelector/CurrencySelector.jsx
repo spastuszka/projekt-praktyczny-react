d460a10e2d97f94cc08483e1660211ec0295bf9c
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2024-08-31 08:33:17 +0200

    Added state when we change option in select value

--- front-end/src/components/CurrencySelector/CurrencySelector.jsx
+++ front-end/src/components/CurrencySelector/CurrencySelector.jsx
@@ -1,9 +1,16 @@
 import styles from './CurrencySelector.module.css'
 import { CURRENCIES } from '../../constants/currencies'
+import { useState } from 'react'
 
 export function CurrencySelector() {
+  const [currency, setCurrency] = useState(CURRENCIES.PLN)
   return (
-    <select className={styles.currencySelector}>
+    <select
+      onChange={(e) => {
+        setCurrency(e.currentTarget.value)
+      }}
+      className={styles.currencySelector}
+    >
       <option value={CURRENCIES.PLN}>{CURRENCIES.PLN}</option>
       <option value={CURRENCIES.USD}>{CURRENCIES.USD}</option>
     </select>
